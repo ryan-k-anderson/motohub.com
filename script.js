a67173796152324e97603d649b412d1f538e1dc7
Ryan Kristopher Anderson <78626914+ryan-k-anderson@users.noreply.github.com>
2022-11-14 04:16:05 -0500

    JS state dropdown logic completed

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let state = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'Tennessee', 'Texas', 'Utah', ' Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
-const selectState = () =>{
-    let selectedState = 0;
-    for(selectedState = 0; selectedState <= state.length; selectedState++){
-        console.log(state[selectedState])
-    }
-}
-    const selection = selectState()
-    console.log(selection)
+let s = 0
+for(s=0; s <= state.length -1;s++){
+console.log(state[s])
+const option = document.createElement('option')
+const innerOption = document.createTextNode(`${state[s]}`)
+option.appendChild(innerOption)
 
-// <option> state[index] </option>
-//Create option elements with the state values incrementing by 1
-//state[0]
-//state[1]
-//state[2]
-document.getElementById('state')
+const element = document.getElementById('state')
+element.appendChild(option)
+}
